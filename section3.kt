0be9fa01aa00ade51fdf793b2efd3d470b94bff4
matsui mai <my.mai521@gmail.com>
2021-02-24 00:30:37 +0900

    3-4まで

---
+++ section3.kt
@@ -0,0 +1,34 @@
+/**
+ * We declare a package-level function main which returns Unit and takes
+ * an Array of strings as a parameter. Note that semicolons are optional.
+ */
+
+class Rational(val n: Int, val d: Int) {
+    
+    init {
+        require(d != 0, {"denominatorに0はダメです"})
+    }
+    
+    private val g = gcd(Math.abs(n), Math.abs(d))
+    val numerator: Int = n / g
+    val denominator: Int = d / g
+    
+    override fun toString(): String = "${numerator}/${denominator}"
+    
+    tailrec private fun gcd(a: Int, b: Int): Int =
+    	if (b == 0) a
+    	else gcd(b, a % b)
+    
+}
+
+fun main(args: Array<String>) {
+    var half = Rational(1,2)
+    println(half.denominator)
+    println(half)
+    println(half.toString())
+    
+    println(Rational(17,17))
+    println(Rational(50,100))
+    
+    println(Rational(1,0))
+}
