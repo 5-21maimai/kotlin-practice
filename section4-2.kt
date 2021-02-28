a21c18e808d531b404d2045ffa047e9ace26bd39
matsui mai <my.mai521@gmail.com>
2021-02-28 22:31:36 +0900

    4章完了

--- section4-2.kt
+++ section4-2.kt
@@ -3,6 +3,14 @@
  * an Array of strings as a parameter. Note that semicolons are optional.
  */
 
+class MyIterator {
+    operator fun hasNext(): Boolean = Math.random() < 0.5
+    operator fun next(): String = "Hello"
+}
+
+class MyIterable {
+    operator fun iterator() = MyIterator()
+}
 
 fun main(args: Array<String>) {
     if(true) {
@@ -72,7 +80,52 @@ fun main(args: Array<String>) {
             score2 >= 80 -> 'B'
             score2 >= 70 -> 'C'
             score2 >= 60 -> 'D'
-            else         -> 'F'
+            else		 -> 'F'
         }
     println(grade2)
+    
+    var count = 3
+    while(count-- > 0) {
+        println("Hello")
+    }
+    
+    do {
+        println("Hello2")
+        val next = Math.random() < 0.5
+    } while(next)
+    
+    for (x in arrayOf(1, 2, 3)) {
+        println(x)
+    }
+    
+    val names = listOf("foo", "bar", "baz")
+    for(name in names) {
+        println(name)
+    }
+    
+    for(i in 1..10 step 2) {
+        println(i)
+    }
+    
+    for(item in MyIterable()) {
+        println(item)
+    }
+    
+    var barIndex = -1
+    for(index in names.indices) {
+        if(names[index] == "bar") {
+            barIndex = index
+            break
+        } 
+    }
+    println(barIndex)
+    println("===========")
+    
+    loop@ for(x in 1..10) {
+        println(x)
+        for(y in 1..10) {
+            print(y)
+            if(y == 5) break@loop
+        }
+    }
 }
