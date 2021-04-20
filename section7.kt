e9130498dbc602724d4e0d26140242e5b12d2f49
matsui mai <my.mai521@gmail.com>
2021-04-20 23:34:36 +0900

    7-5まで

--- section7.kt
+++ section7.kt
@@ -8,49 +8,63 @@ interface Bucket {
     fun drainAway()
     fun pourTo(that: Bucket)
     
-    fun getCapacity(): Int
-    fun getQuantity(): Int
-    fun setQuantity(quantity: Int)
+    val capacity: Int
+    val quantity: Int
 }
 
 
 fun main() {
     val bucket1 = createBucket(7)
     val bucket2 = createBucket(4)
+    val bucket3 = BucketImpl(6)
     
     bucket1.fill()
     bucket2.fill()
+    bucket3.fill()
     
-    println(bucket1.getQuantity())
-    println(bucket2.getQuantity())
+    println(bucket1.quantity)
+    println(bucket2.quantity)
+    println(bucket3.quantity)
     
     bucket1.drainAway()
+    bucket3.drainAway()
     
-    println(bucket1.getQuantity())
-    println(bucket2.getQuantity())
+    println(bucket1.quantity)
+    println(bucket2.quantity)
+    println(bucket3.quantity)
     
 }
 
-fun createBucket(capacity: Int): Bucket = object : Bucket {
-    var _quantity: Int = 0
+fun createBucket(_capacity: Int): Bucket = object : Bucket {
+    override var quantity: Int = 0
+    override val capacity = _capacity
     
     override fun fill() {
-        setQuantity(getCapacity())
+        quantity = capacity
     }
     
     override fun drainAway() {
-        setQuantity(0)
+        quantity = 0
     }
     
     override fun pourTo(that: Bucket) {
         
     }
+}
+
+class BucketImpl(_capacity: Int): Bucket {
+    override var quantity: Int = 0
+    override val capacity = _capacity
     
-    override fun getCapacity(): Int = capacity
+    override fun fill() {
+        quantity = capacity
+    }
     
-    override fun getQuantity(): Int = _quantity
+    override fun drainAway() {
+        quantity = 0
+    }
     
-    override fun setQuantity(quantity: Int) {
-        _quantity = quantity
+    override fun pourTo(that: Bucket) {
+        
     }
 }
