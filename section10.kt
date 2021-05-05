005c8555e6cb70c2b7e13c16bda8938cdf01dea9
matsui mai <my.mai521@gmail.com>
2021-05-05 23:26:56 +0900

    10-2

---
+++ section10.kt
@@ -0,0 +1,61 @@
+/**
+ * You can edit, run, and share this code. 
+ * play.kotlinlang.org 
+ */
+
+interface Greeter {
+    val language: String
+    fun sayHello(target: String)
+}
+
+class EnglishGreeter: Greeter {
+    override val language = "English"
+    override fun sayHello(target: String) {
+        println("Hello, $target!")
+    }
+}
+
+fun main(args: Array<String>) {
+    EnglishGreeter().sayHello("Kotlin")
+    
+}
+
+interface Foo {
+    fun execute()
+}
+
+interface Bar {
+    fun execute()
+}
+
+class FooBar: Foo, Bar {
+    override fun execute() {
+        println("Foobar")
+    }
+}
+
+open class SuperClass {
+    open fun execute() {
+        println("SuperClass")
+    }
+}
+
+class FooSubclass: SuperClass(), Foo
+
+interface Hoge {
+    fun execute() {
+        println("Hoge")
+    }
+}
+
+interface Fuga {
+    fun execute() {
+        println("Fuga")
+    }
+}
+
+class HogeFuga: Hoge, Fuga {
+    override fun execute() {
+        super<Hoge>.execute()
+    }
+}
